f8e93efca9d72a78a04e15e4ce8dacb37e77d25f
smore_d <kurtqa22@gmail.com>
2025-05-12 18:56:33 -0700

    Animations updated and implemented
    Idle and walk animation has been completed and sprite sheets have been fixed(PIXLE UP)

--- Characters/character_body_2d.gd
+++ Characters/character_body_2d.gd
@@ -1,14 +1,44 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 100
+@onready var animated_sprite = $AnimatedSprite2D
+
+enum Direction { LEFT, RIGHT, UP, DOWN}
+var last_direction: Direction = Direction.DOWN
+
+
 
 func _physics_process(_delta):
 	#get input direction
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
-	)
+	).normalized()
 	#print(input_direction)
+	#NEW ANIMATIONS MUST TAKE PRIORITES OVER THESE IE HIT ANS DASH AND MAGIC
+	if (input_direction.x > 0):
+		animated_sprite.play("Walk D")
+		last_direction = Direction.RIGHT
+	elif (input_direction.x < 0):
+		animated_sprite.play("Walk A")
+		last_direction = Direction.LEFT
+	elif (input_direction.y < 0):
+		animated_sprite.play("Walk W")
+		last_direction = Direction.UP
+	elif (input_direction.y > 0):
+		animated_sprite.play("Walk S")
+		last_direction = Direction.DOWN
+
+	else:
+		match (last_direction):
+			Direction.LEFT:
+				animated_sprite.play("Idle A")
+			Direction.RIGHT:
+				animated_sprite.play("Idle D")
+			Direction.UP:
+				animated_sprite.play("Idle W")
+			Direction.DOWN:
+				animated_sprite.play("Idle S")
 	
 	#update velocity
 	velocity = input_direction * move_speed
